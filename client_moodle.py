3e2c0b45dc3b155f04e1c2633473ead30214ff5d
Maxslide <manaskabre99@gmail.com>
2020-03-21 21:55:02 +0530

    First commit

--- client_moodle.py
+++ client_moodle.py
@@ -1,13 +1,15 @@
 import json
 import requests
 import numpy as np
-
+import random
 ######### DO NOT CHANGE ANYTHING IN THIS FILE ##################
 API_ENDPOINT = 'http://10.4.21.147'
 PORT = 3000
 MAX_DEG = 11
-
+arr = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
 #### functions that you can call
+# def modify_decimal(test_arr):
+    
 def get_errors(id, vector):
     """
     returns python array of length 2 
@@ -43,15 +45,54 @@ def send_request(id, vector, path):
 
     return response
 
-
+def BitComplement():
+    with open('data.json') as f:
+        data = json.load(f)
+    vector = random.choice(data)["arr"]
+    print(vector)
+    for i in range(1,len(vector)):
+        for j in range(random.choice(list(range(3)))):
+            temp = list('{:.27f}'.format(vector[i]))
+            mini = 3
+            for k in range(3,len(temp)):
+                if(temp[k] != '0'):
+                    mini = k
+                    break
+            maxi = len(temp)
+            if(i in list(range(5))):
+                maxi = 17
+            if(i in list(range(5,9))):
+                maxi = 23
+            mid = (mini+maxi)/2
+            # for i in range(len(temp) -1, 2):
+            #     if(temp[i])
+            rand = random.choice(list(range(int((mini+mid)/2),int((maxi+mid)/2)))) 
+            temp[rand] = str(random.choice(list(range(10))))
+            # print(i)
+            vector[i] = float(''.join(temp))
+    return vector 
+        
+def Add_To_File(arr,err):
+    with open('data.json') as f:
+        data = json.load(f)
+    y = {"arr" : arr, "Terr": err[0], "Verr" : err[1]}
+    data.append(y)
+    with open('data.json','w') as f:
+        json.dump(data,f,indent=4)
+    print("Added to the file", y)
+    return
 if __name__ == "__main__":
     """
     Replace "i0ZxSBn9KTktTOfG5xlLZ9CrNY2hEhg8SnLisL4CHNHGtYuqLf" with your secret ID and just run this file 
     to verify that the server is working for your ID.
     """
-    err = get_errors('i0ZxSBn9KTktTOfG5xlLZ9CrNY2hEhg8SnLisL4CHNHGtYuqLf', [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12])
+    vector = BitComplement()
+    err = get_errors('i0ZxSBn9KTktTOfG5xlLZ9CrNY2hEhg8SnLisL4CHNHGtYuqLf', vector)
     assert len(err) == 2
     print(err)
+    if(len(err) == 2 and len(arr) == 11):
+        Add_To_File(vector,err)
+    
 
     # submit_status = submit('i0ZxSBn9KTktTOfG5xlLZ9CrNY2hEhg8SnLisL4CHNHGtYuqLf', list(-np.arange(0,1.1,0.1)))
     # assert "submitted" in submit_status
